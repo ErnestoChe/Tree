83f124b1ed4b6d3b77f9d725550d44ecd6467754
Ernest <ernestfilippov@mail.ru>
2019-06-21 13:02:11 +0300

    modified depth

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,12 +9,13 @@ public class Main {
     public static void main(String[] args) {
         int[] numbers = {40, 20, 60, 10, 30, 50, 70, 5, 45, 55, 65, 75};
 
-        aBST abst = new aBST(4);
+        aBST abst = new aBST(3);
         for (int i:numbers) {
             System.out.println(i+"\t"+abst.AddKey(i));
         }
         System.out.println();
 
         System.out.println(abst.AddKey(50));
+        System.out.println(abst.FindKeyIndex(10));
     }
 }
